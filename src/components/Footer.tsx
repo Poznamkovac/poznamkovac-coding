bf08cd0a0e1ccbda351f83fd2646cb2a13a1c0f6
Kevin Svitač <kevin@svit.ac>
2024-08-12 21:22:15 +0200

    link na GitHub

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-indigo-900 p-4 mt-8">
+    <footer className="p-4 mt-8 bg-indigo-900">
       <div className="container mx-auto text-center">
-        <p>&copy; 2024 Poznámkovač. All rights reserved.</p>
+        <p>&copy; 2024 Poznámkovač</p>
+        <Link to="https://github.com/SKevo18/poznamkovac-webulohy" className="hover:underline">GitHub</Link>
       </div>
     </footer>
   );
