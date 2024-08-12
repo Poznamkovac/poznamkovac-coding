eb714ace34b3aa43c023b176de90ef9464991966
Kevin Svitač <kevin@svit.ac>
2024-08-12 21:23:58 +0200

    lepší link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
     <footer className="p-4 mt-8 bg-indigo-900">
       <div className="container mx-auto text-center">
         <p>&copy; 2024 Poznámkovač</p>
-        <Link to="https://github.com/SKevo18/poznamkovac-webulohy" className="hover:underline">GitHub</Link>
+        <Link to="https://github.com/SKevo18/poznamkovac-webulohy" className="text-blue-300 hover:text-blue-200 hover:underline">Zdrojový kód (GitHub)</Link>
       </div>
     </footer>
   );
