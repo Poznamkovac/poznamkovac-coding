07a6ee5a9a77f6c7a284b07c9ffaad0cff392e0d
Kevin Svitač <kevin@svit.ac>
2024-08-12 20:38:00 +0200

    to chunkovanie `monaco-edito` som si rozmyslel načítajú sa všetky a spomaľuje to stránku

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-
-// rýchly fix pre 4 MB monaco-editor
-// chunkov bude viac (pretože stále zahŕňame všetky jazyky, atď...),
-// ale na stránke sa načítajú iba tie ktoré potrebujeme (HTML, CSS, JS)
-let monacoChunk = 0;
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -15,10 +9,6 @@ export default defineConfig({
       output: {
         manualChunks: (id: string) => {
           if (id.includes("node_modules")) {
-            if (id.includes("monaco-editor")) {
-              monacoChunk++;
-              return `monaco-editor-${monacoChunk}`;
-            }
             return id.split("node_modules/")[1].split("/")[0];
           }
         },
