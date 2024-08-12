790e1706c4ff443c3792e132ce67c6ec956431b4
Kevin Svitač <kevin@svit.ac>
2024-08-12 20:38:48 +0200

    jeden veľký vendor chunk čo už

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       output: {
         manualChunks: (id: string) => {
           if (id.includes("node_modules")) {
-            return id.split("node_modules/")[1].split("/")[0];
+            return "vendor";
           }
         },
       },
