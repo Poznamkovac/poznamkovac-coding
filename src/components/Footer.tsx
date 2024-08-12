277aa4ae1dd98db4c842a1f261163802c1977d89
Kevin Svitač <kevin@svit.ac>
2024-08-12 21:34:44 +0200

    gh odkaz na novom okne

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => {
   return (
     <footer className="p-4 mt-8 bg-indigo-900">
       <div className="container mx-auto text-center">
         <p>&copy; 2024 Poznámkovač</p>
-        <Link to="https://github.com/SKevo18/poznamkovac-webulohy" className="text-blue-300 hover:text-blue-200 hover:underline">Zdrojový kód (GitHub)</Link>
+        <Link target="_blank" to="https://github.com/SKevo18/poznamkovac-webulohy" className="text-blue-300 hover:text-blue-200 hover:underline">
+          Zdrojový kód (GitHub)
+        </Link>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
