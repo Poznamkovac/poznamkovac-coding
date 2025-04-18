2d9b6a6b6defec5a08d2a6c4eb8c730656b69f81
Kevin Svitač <kevin@svit.ac>
2025-04-18 11:48:05 +0200

    upraviť implementáciu bubble sortu aby sa zhodovala s poznámkovačom

--- public/data/sk/challenges/python/.template/files/test.py
+++ public/data/sk/challenges/python/.template/files/test.py
@@ -1,5 +1,5 @@
 try:
-    from main import funkcia  # type: ignore
+    from main import funkcia  # type: ignore  # noqa: F401
 except ImportError:
     raise ImportError("Funkcia `funkcia()` nie je definovaná.")
 
--- public/data/sk/challenges/python/algoritmy/triedenie/1/solution/main.py
+++ public/data/sk/challenges/python/algoritmy/triedenie/1/solution/main.py
@@ -1,7 +1,7 @@
 def bubble_sort(pole):
     n = len(pole)
-    for i in range(n):
-        for j in range(n-i-1):
+    for i in range(n-1, -1, -1):
+        for j in range(i):
             if pole[j] > pole[j+1]:
                 pole[j], pole[j+1] = pole[j+1], pole[j]
 
