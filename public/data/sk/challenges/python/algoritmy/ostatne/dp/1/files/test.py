460ec9dc7e982f2b704abe427accbbf07d334e45
Kevin Svitač <kevin@svit.ac>
2025-04-20 19:26:59 +0200

    feedback pre fibonacci testy

--- public/data/sk/challenges/python/algoritmy/ostatne/dp/1/files/test.py
+++ public/data/sk/challenges/python/algoritmy/ostatne/dp/1/files/test.py
@@ -9,9 +9,14 @@ except ImportError:
     raise ImportError("Premenná s medzivýsledkami `dp` nie je definovaná.")
 
 fibonacci(20)
-assert dp[0] == 0
-assert dp[1] == 1
-assert dp[5] == 5
-assert dp[10] == 55
-assert dp[15] == 610
-assert dp[20] == 6765
+try:
+    assert dp[0] == 0
+    assert dp[1] == 1
+    assert dp[5] == 5
+    assert dp[10] == 55
+    assert dp[15] == 610
+    assert dp[20] == 6765
+except AssertionError:
+    raise AssertionError("Premenná `dp` obsahuje nesprávne hodnoty.")
+
+print("OK")
